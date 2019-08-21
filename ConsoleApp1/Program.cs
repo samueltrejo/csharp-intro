39a7942f6d912554077b5f277c867823f1dd9f7e
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-08-20 20:30:16 -0500

    console app intro

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myVariable = "Hello World!";
+            DateTime myDateTime = new DateTime();
+
+            string myStringVariable;
+            myStringVariable = "steve";
+
+            int myIntVariable;
+            myIntVariable = 29384723;
+
+            decimal myDecimalVariable;
+            myDecimalVariable = 12345.3434m;
+
+            bool myBool = false;
+
+            if (myBool) {
+                Console.WriteLine("We will never hit htis code");
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"we're on iteration {i}");
+            }
+
+
+
+            var myThing = new { Stuff = "steve" };
+
+            List<string> myStrings = new List<string>();
+            myStrings.Add("blerg");
+            myStrings.Add("blarg");
+            myStrings.Add("blirg");
+
+            foreach(var myString in myStrings)
+            {
+                Console.WriteLine(myString);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
